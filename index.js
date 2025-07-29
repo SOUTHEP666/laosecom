6481534e049fd358e57fcb82530fddb4aadfd44e
SOUTHEP666 <southep_sichan@hotmail.com>
2025-07-29 22:12:19 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// index.js
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.js';
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+
+app.get('/', (req, res) => {
+  res.send('购物网站后端运行中 🚀');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`服务器运行在 http://localhost:${PORT}`);
+});
