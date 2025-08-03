3e8977c41895c3aba8f7231ca573c5193a3a8cdb
SOUTHEP666 <southep_sichan@hotmail.com>
2025-08-04 00:20:34 +0800

    fix: add

--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createProduct, getAllProducts, updateProduct, reviewProduct } from '../controllers/productController.js';
+import { createProduct, getAllProducts, getProductById, updateProduct, reviewProduct } from '../controllers/productController.js';
 
 const router = express.Router();
 
 router.post('/', createProduct);
 router.get('/', getAllProducts);
+router.get('/:id', getProductById);
 router.put('/:id', updateProduct);
 router.post('/:id/review', reviewProduct); // 审核流程接口
 
