7917f1d01eae38ad54d337597edc8d7009b87593
SOUTHEP666 <southep_sichan@hotmail.com>
2025-08-06 17:27:33 +0800

    fix: add

--- utils/stripe.js
+++ utils/stripe.js
@@ -1,9 +1,7 @@
-import Stripe from "stripe";
-import dotenv from "dotenv";
-dotenv.config();
+import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2022-11-15",
+  apiVersion: '2022-11-15', // 举例，具体版本看你项目
 });
 
 export default stripe;
