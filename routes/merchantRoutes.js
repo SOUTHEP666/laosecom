dca1975ce066b2d048f386974679a285e67c180f
SOUTHEP666 <southep_sichan@hotmail.com>
2025-08-06 22:06:30 +0800

    fix: add

--- routes/merchantRoutes.js
+++ routes/merchantRoutes.js
@@ -1,2 +1,5 @@
-import express from "express"; import { applyMerchant } from "../controllers/merchantController.js"; const router = express.Router(); 
- router.post("/apply", applyMerchant); export default router;
+import express from "express"; 
+import { applyMerchant } from "../controllers/merchantController.js"; 
+const router = express.Router(); 
+router.post("/apply", applyMerchant); 
+export default router;
