f2a6ffe75c5bfe82bc1eca2b9c06cf3ce55d15d5
SOUTHEP666 <southep_sichan@hotmail.com>
2025-08-08 11:07:51 +0800

    fix: add

--- routes/products.js
+++ routes/products.js
@@ -1,5 +1,5 @@
 import express from "express";
-import authMiddleware from "../middleware/auth.js"; // 验证token及角色
+import authMiddleware from "../middlewares/auth.js"; // 验证token及角色
 import { pool } from "../config/db.js";
 
 const router = express.Router();
