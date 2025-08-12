f77af27afd3631a4e2a45d42f418448aa735b8b9
SOUTHEP666 <southep_sichan@hotmail.com>
2025-08-12 23:11:15 +0800

    fix: add

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.use(cors({
     }
   },
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],  // 加上 PATCH 方法
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
